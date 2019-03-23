2337b68aae7a7b2a4d19ca5f40abafcf6d3a8b59
jrizza88 <jetorres88@gmail.com>
2019-03-22 22:21:54 -0400

    refactored routers into separate folders and files

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
 const express = require('express');
+
+const projectRoutes = require('./data/routes/projectRoutes');
+const actionRoutes = require('./data/routes/actionRoutes');
+
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 
@@ -9,26 +13,26 @@ const server = express();
 
 server.use(express.json());
 
+server.use('/api/projects', projectRoutes);
+server.use('/api/actions', actionRoutes);
+
+
 // made this endpoint to make sure endpoints work correctly on browser. 
-server.get('/api/projects', async (req, res) => {
-    try {
-        const showProjects = await db('projects')
-        res.status(200).json(showProjects)
-    } catch (err){
-        res.status(500).json(err)
-    }
+server.get('/', (req, res) => {
+    res.send(`<h2>Welcome to the Project and Actions application! \n
+    Please type /api/projects or /api/actions to view some of the endpoints! </h2>`)
 });
 
-server.post('/api/projects', async (req, res) => {
-    try {
-        const project = req.body;
-        const [id] = await db('projects').insert(project)
-        const newProject = await db('projects').where({id}).first()
-        res.status(201).json(newProject)
-    } catch (err){
-        res.status(500).json(err)
-    }
-});
+// server.post('/api/projects', async (req, res) => {
+//     try {
+//         const project = req.body;
+//         const [id] = await db('projects').insert(project)
+//         const newProject = await db('projects').where({id}).first()
+//         res.status(201).json(newProject)
+//     } catch (err){
+//         res.status(500).json(err)
+//     }
+// });
 
 const port = process.env.PORT || 7777;
 
