d68d0fa8db2cdef311d6af47ec8642492ce6a168
jrizza88 <jetorres88@gmail.com>
2019-03-22 22:30:17 -0400

    successfully added post request for new actions

--- data/routes/actionRoutes.js
+++ data/routes/actionRoutes.js
@@ -5,5 +5,24 @@ const knexConfig = require('../../knexfile');
 
 const db = knex(knexConfig.development);
 
+router.get('/', async (req, res) => {
+    try {
+        const showActions = await db('actions')
+        res.status(200).json(showActions)
+    } catch (err){
+        res.status(500).json(err)
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const action = req.body;
+        const [id] = await db('actions').insert(action)
+        const newAction = await db('actions').where({id}).first()
+        res.status(201).json(newAction)
+    } catch (err){
+        res.status(500).json(err)
+    }
+});
 
 module.exports = router;
